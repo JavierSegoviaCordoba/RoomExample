1436c149919ad18c21b198aefb0e84b79738bdc6
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2019-04-05 01:25:26 +0200

    Changed ViewHolder name

--- app/src/main/java/com/javisc/roomexample/ui/photofragment/PhotoAdapter.kt
+++ app/src/main/java/com/javisc/roomexample/ui/photofragment/PhotoAdapter.kt
@@ -12,20 +12,20 @@ import com.javisc.roomexample.extension.glideCrossFadeCircle
 import kotlinx.android.synthetic.main.item_photo.view.*
 
 
-class PhotoAdapter : ListAdapter<Photo, PhotoAdapter.MovieViewHolder>(TaskDiffCallback()) {
+class PhotoAdapter : ListAdapter<Photo, PhotoAdapter.PhotoViewHolder>(TaskDiffCallback()) {
 
     private var list: List<Photo>? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return MovieViewHolder(inflater.inflate(R.layout.item_photo, parent, false))
+        return PhotoViewHolder(inflater.inflate(R.layout.item_photo, parent, false))
     }
 
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) =
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) =
         holder.bind(getItem(position))
 
 
-    class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bind(photo: Photo) {
             with(itemView) {
