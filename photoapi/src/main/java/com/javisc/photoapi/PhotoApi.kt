1da175169a67e51d1721a98ba5cfa22ae56be737
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2019-04-07 22:00:22 +0200

    Fixed koin isolation
    Fixed koin isolation

--- photoapi/src/main/java/com/javisc/photoapi/PhotoApi.kt
+++ photoapi/src/main/java/com/javisc/photoapi/PhotoApi.kt
@@ -2,20 +2,25 @@ package com.javisc.photoapi
 
 import com.javisc.photoapi.di.modules
 import com.javisc.photoapi.repository.PhotoApiRepo
+import org.koin.core.Koin
 import org.koin.core.KoinComponent
-import org.koin.core.context.loadKoinModules
 import org.koin.core.inject
+import org.koin.dsl.koinApplication
 
-class PhotoApi : KoinComponent {
-
-    companion object {
-        init {
-            loadKoinModules(modules)
-        }
-    }
+class PhotoApi : PhotoApiKoinComponent() {
 
     private val photoApiRepo by inject<PhotoApiRepo>()
 
     suspend fun getPhoto(id: Int) = photoApiRepo.getPhoto(id)
+
+}
+
+abstract class PhotoApiKoinComponent : KoinComponent {
+
+    companion object {
+        val koinApp = koinApplication { modules(modules) }.koin
+    }
+
+    override fun getKoin(): Koin = koinApp
 
 }
